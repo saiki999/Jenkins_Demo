b47bc192a7feb0c6f5dfe306621de1bb198de0a8
Saikiran Kommanaboina <Saikiran Kommanaboina>
2017-02-18 20:02:36 -0500

    basic jenkin exapmle

---
+++ hello.java
@@ -0,0 +1,20 @@
+
+
+
+public class hello{
+	
+	public static void main(String[] args)	
+	{
+		
+		for(int i=1;i<5;i++)
+		{
+			
+			
+			System.out.println("Hello  World" + i);
+		}
+		
+	}
+	
+	
+	
+}
